0de21d2a573d6fa1e41e1cbe67ccafc72494f35a
mtngz <70101199+mtngz@users.noreply.github.com>
2021-02-16 18:00:32 +0100

    create director component and update components

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
 import { GetAllMoviesService } from "../fetch-api-data.service";
+import { MovieDirectorComponent } from '../movie-director/movie-director.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +11,10 @@ import { GetAllMoviesService } from "../fetch-api-data.service";
 
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: GetAllMoviesService) { }
+  constructor(
+    public fetchApiData: GetAllMoviesService,
+    public dialog: MatDialog
+    ) { }
 
 ngOnInit(): void {
   this.getMovies();
@@ -20,6 +25,19 @@ getMovies(): void {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openDirectorDialog(
+    Name: string,
+    Bio: string,
+    Birth: string,
+    Death: string
+  ): void {
+    this.dialog.open(MovieDirectorComponent, {
+      data: { Name, Bio, Birth, Death },
+      width: '600px',
+      height: '250px',
     });
   }
 }
---
+++ src/app/movie-director/movie-director.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-movie-director',
+  templateUrl: './movie-director.component.html',
+  styleUrls: ['./movie-director.component.scss']
+})
+
+export class MovieDirectorComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+      Birth: string;
+      Death: string;
+    }
+  ) {}
+}
