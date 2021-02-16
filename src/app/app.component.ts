3f69aa4906cb4fb84f92f1f0af7b8a73475bd90a
mtngz <70101199+mtngz@users.noreply.github.com>
2021-02-16 15:39:31 +0100

    delete unused code in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from "./user-login-form/user-login-form.component";
-import { MatDialog } from '@angular/material/dialog';
-import { MovieCardComponent } from './movie-card/movie-card.component';
+
 
 @Component({
   selector: 'app-root',
@@ -12,25 +9,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
-    });
-  }
-// This is the function that will open the dialog when login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
-// This is the function that will open the dialog when all Movies button is clicked
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
